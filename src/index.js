f03134db9b084b5acc9ddfa21b8646e1e886a992
Eliška Buociková <buocikovaeliska@gmail.com>
2023-04-29 12:44:30 +0200

    vytvoření jednotlivých komponent projektu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
+import { Header } from './components/Header/Header.js';
+import { Banner } from './components/Banner/Banner.js';
+import { Menu } from './components/Menu/Menu.js';
+import { Gallery } from './components/Gallery/Gallery.js';
+import { Contact } from './components/Contact/Contact.js';
+import { Footer } from './components/Footer/Footer.js';
+
 console.log('funguju!');
+
+const pageElm = document.createElement('div');
+pageElm.classList.add('page');
+
+const mainElm = document.createElement('main');
+mainElm.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElm.append(Header(), mainElm, Footer());
+
+document.querySelector('#app').append(pageElm);
